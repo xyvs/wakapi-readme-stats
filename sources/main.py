4c002c24597683472c5d2f28b7f0adcf0d4c0daf
Fran <fran@xyvs.io>
2024-11-03 01:47:48 -0500

    feature: Update followers location

--- sources/main.py
+++ sources/main.py
@@ -82,15 +82,6 @@ async def get_short_github_info() -> str:
     :returns: String representation of the info.
     """
 
-    if EM.SHOW_FOLLOWERS:
-        DBM.i("Adding profile followers...")
-        stats = f"**🐱 {FM.t('Awesome People!')}** \n\n"
-        data = GHM.USER.get_followers()
-        for user in data:
-            username = user.login.replace("-", "--")
-            stats += f"![@{user.login}](https://img.shields.io/badge/@{username}-black?style=plastic&logo=github&logoColor=fff&link={user.html_url})"
-        stats += f"\n\nIf you follow my account you can appear on this list. *This list updates every 12h*\n\n"
-
     DBM.i("Adding short GitHub info...")
     stats += f"**🐱 {FM.t('My GitHub Data')}** \n\n"
 
@@ -174,6 +165,16 @@ async def get_stats() -> str:
     else:
         yearly_data, commit_data = dict(), dict()
         DBM.w("User yearly data not needed, skipped.")
+
+    
+    if EM.SHOW_FOLLOWERS:
+        DBM.i("Adding profile followers...")
+        stats = f"**⭐ {FM.t('Awesome People!')}** \n\n"
+        data = GHM.USER.get_followers()
+        for user in data:
+            username = user.login.replace("-", "--")
+            stats += f"![@{user.login}](https://img.shields.io/badge/@{username}-black?style=plastic&logo=github&logoColor=fff&link={user.html_url})"
+        stats += f"\n\nIf you follow my account you can appear on this list. *This list updates every 12h*\n\n"
 
     if EM.SHOW_TOTAL_CODE_TIME:
         DBM.i("Adding total code time info...")
